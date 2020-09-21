323f8a7d3578cb2ed040b02c50f7d64a33065cc8
Maguirre123 <71620017+Maguirre123@users.noreply.github.com>
2020-09-20 22:11:17 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showLeds(`
+        # # . . #
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(100)
+})
